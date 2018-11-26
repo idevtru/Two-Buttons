ed2b209790a506df79a47f64257ca45ebec8a568
Rich Castagna <idev.tru@gmail.com>
2018-11-25 20:21:24 -0500

    Added Set Text and Clear Text buttons which update a label's text based on text entered in text field. Clear Text button clears label and text field.

--- Two Buttons/ViewController.swift
+++ Two Buttons/ViewController.swift
@@ -10,11 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func setTextButtonTapped(_ sender: UIButton) {
+        textLabel.text = textField.text
+    }
+    
+    @IBAction func clearTextButtonTapped(_ sender: UIButton) {
+        textField.text = ""
+        textLabel.text = ""
+    }
+    
 
 }
 

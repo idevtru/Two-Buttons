2b15ae19cc7685d00b05545a871b5564ff634f0c
Rich Castagna <idev.tru@gmail.com>
2018-11-25 19:24:11 -0500

    Initial Commit

---
+++ Two Buttons/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Two Buttons
+//
+//  Created by Richard Castagna on 11/25/18.
+//  Copyright © 2018 Richard Castagna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
